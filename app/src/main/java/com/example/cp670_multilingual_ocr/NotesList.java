db4b07db56092300558c751294aca23679d864d5
David Moreno-Bautista <more5580@mylaurier.ca>
2024-06-14 22:59:30 -0400

    Add navigation toolbar

--- app/src/main/java/com/example/cp670_multilingual_ocr/NotesList.java
+++ app/src/main/java/com/example/cp670_multilingual_ocr/NotesList.java
@@ -9,7 +9,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class NotesList extends AppCompatActivity {
+public class NotesList extends MainActivity {
     private static final String TAG = "NotesList";
 
     @Override
@@ -19,11 +19,15 @@ public class NotesList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_notes_list);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public int getLayoutResource(){
+        return R.layout.activity_notes_list;
     }
 }
--- app/src/main/java/com/example/cp670_multilingual_ocr/Settings.java
+++ app/src/main/java/com/example/cp670_multilingual_ocr/Settings.java
@@ -9,18 +9,24 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import android.util.Log;
 
-public class Settings extends AppCompatActivity {
+public class Settings extends MainActivity {
     private static final String TAG = "Settings";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.i(TAG, "inside onCreate");
+
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_settings);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public int getLayoutResource(){
+        return R.layout.activity_settings;
     }
 }
