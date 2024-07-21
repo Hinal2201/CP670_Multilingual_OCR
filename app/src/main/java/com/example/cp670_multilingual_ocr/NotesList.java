56faa9fd3bb6e1fc0ca4254b1c593bf69e4fff66
David Moreno-Bautista <71296308+damoba@users.noreply.github.com>
2024-07-21 04:26:06 -0400

    Clean up some warnings

--- app/src/main/java/com/example/cp670_multilingual_ocr/NotesList.java
+++ app/src/main/java/com/example/cp670_multilingual_ocr/NotesList.java
@@ -6,19 +6,12 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
